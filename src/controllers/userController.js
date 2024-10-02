88b9c59a35f35c278146d560c1d58e736cc38ae8
kwangmon <kwangmony@gmail.com>
2024-10-02 22:40:30 +0900

    mod: modify planning routes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,6 @@
 export const join = (req, res) => res.send("Join");
 export const editUser = (req, res) => res.send("edit");
 export const deleteUser = (req, res) => res.send("delete");
+export const login = (req, res) => res.send("login");
+export const logout = (req, res) => res.send("logout");
+export const see = (req, res) => res.send("see");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
 
-import { editUser, deleteUser } from "../controllers/userController";
+import { editUser, deleteUser, logout, see } from "../controllers/userController";
 
 const userRouter = express.Router();
 
+userRouter.get("/logout", logout);
 userRouter.get("/edit", editUser);
 userRouter.get("/delete", deleteUser);
+userRouter.get("/:id", see);
 
 export default userRouter;
